38e01924bb9198d4242f9ff26e388e838cef5102
jeongsooyoung <code.ameba@gmail.com>
2020-06-04 15:40:50 +0900

    come on netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `https://codeameba.github.io/`,
+        start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
